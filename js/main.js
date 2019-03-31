8a92eee565fe32fc486b37d4f2a558f47ef06544
Kashyap Chikhaliya (KC) <chikhaliyakashyap001@gmail.com>
2019-03-31 09:28:45 +0530

    UUID changes

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ const logToTerminal = (message, type = '') => {
 };
 
 // Obtain configured instance.
-const terminal = new BluetoothTerminal();
+const terminal = new BluetoothTerminal('6E400001-B5A3-F393-E0A9-E50E24DCCA9E','6E400003-B5A3-F393-E0A9-E50E24DCCA9E');
 
 // Override `receive` method to log incoming data to the terminal.
 terminal.receive = function(data) {
