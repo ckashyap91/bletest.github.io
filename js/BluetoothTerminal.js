ce5de56ac09083231af619017cb535736940f7cd
Kashyap Chikhaliya (KC) <chikhaliyakashyap001@gmail.com>
2019-03-31 09:39:54 +0530

    sdsad

--- js/BluetoothTerminal.js
+++ js/BluetoothTerminal.js
@@ -240,7 +240,7 @@ class BluetoothTerminal {
    */
   _requestBluetoothDevice() {
     this._log('Requesting bluetooth device...');
-    this._log('New Code');
+    this._log('New Code12');
     return navigator.bluetooth.requestDevice({
       filters: [],
       acceptAllDevices: true
