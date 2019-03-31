3f8e33bd54b43866fcf99651f21422603b0aef5a
Kashyap Chikhaliya (KC) <chikhaliyakashyap001@gmail.com>
2019-03-31 11:06:15 +0530

    dsfsdfsdf

--- js/BluetoothTerminal.js
+++ js/BluetoothTerminal.js
@@ -222,15 +222,15 @@ class BluetoothTerminal {
    */
   _requestBluetoothDevice() {
     this._log('Requesting bluetooth device...');
-    this._log('New Code with UUID 4');
-    let optionalServices = '6e400001-b5a3-f393-e0a9-e50e24dcca9e'
-    .split(/, ?/).map(s => s.startsWith('0x') ? parseInt(s) : s)
-    .filter(s => s && BluetoothUUID.getService);
+    this._log('New Code with UUID 5');
+    // let optionalServices = '6e400001-b5a3-f393-e0a9-e50e24dcca9e'
+    // .split(/, ?/).map(s => s.startsWith('0x') ? parseInt(s) : s)
+    // .filter(s => s && BluetoothUUID.getService);
 
-    log('Requesting any Bluetooth Device...');
+    //log('Requesting any Bluetooth Device...');
     return navigator.bluetooth.requestDevice({
       acceptAllDevices: true,
-      optionalServices: optionalServices
+      optionalServices: ['6e400001-b5a3-f393-e0a9-e50e24dcca9e']
     }).
         then((device) => {
           this._log('"' + device.name + '" bluetooth device selected');
