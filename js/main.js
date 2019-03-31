2f5df3df9bc6e0833e746d4e361cfee4bce5c885
Kashyap Chikhaliya (KC) <chikhaliyakashyap001@gmail.com>
2019-03-31 09:31:24 +0530

    sss

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ const logToTerminal = (message, type = '') => {
 };
 
 // Obtain configured instance.
-const terminal = new BluetoothTerminal('6E400001-B5A3-F393-E0A9-E50E24DCCA9E','6E400003-B5A3-F393-E0A9-E50E24DCCA9E');
+const terminal = new BluetoothTerminal('6E400001-B5A3-F393-E0A9-e50e24dcca9e','6e400003-b5a3-f393-e0a9-e50e24dcca9e');
 
 // Override `receive` method to log incoming data to the terminal.
 terminal.receive = function(data) {
