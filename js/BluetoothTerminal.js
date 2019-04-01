56461c1279fa371b4b8a10e4727abfe9397f4220
Kashyap Chikhaliya (KC) <chikhaliyakashyap001@gmail.com>
2019-04-02 00:21:58 +0530

    dsadd

--- js/BluetoothTerminal.js
+++ js/BluetoothTerminal.js
@@ -250,7 +250,7 @@ class BluetoothTerminal {
    */
   _requestBluetoothDevice() {
     this._log('Requesting bluetooth device...');
-    this._log('New Code with UUID 500');
+    this._log('New Code with UUID 501');
     // let optionalServices = '6e400001-b5a3-f393-e0a9-e50e24dcca9e'
     // .split(/, ?/).map(s => s.startsWith('0x') ? parseInt(s) : s)
     // .filter(s => s && BluetoothUUID.getService);
@@ -424,7 +424,7 @@ class BluetoothTerminal {
     }
     if (command == 21) {
       this._deviceId = data;
-      var deviceFound = true;
+      var deviceFound = false;
       this._log('21 Receive');
       if (deviceFound) {
         this._sendToDevice(command, 1);
